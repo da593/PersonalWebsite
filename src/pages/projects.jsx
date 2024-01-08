f2a55c23f859729d488267b62e4fef1857a9dcf4
da593 <dylanau99@gmail.com>
2024-01-08 17:08:41 -0500

    Update resume

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import data from '../components/projects/data.json';
 import ProjectCard from '../components/projects/ProjectCard';
 
-const resumeSrc = "https://drive.google.com/file/d/1pgdaqyHKJZKLa-eKSi3BSRHo6-uXDQyX/view?usp=drive_link"
+const resumeSrc = "https://drive.google.com/file/d/1Pbgt8biCFOTbNv6drRFSkzzkLtXEFvCP/view?usp=drive_link"
 
 function Projects() {
     return (
