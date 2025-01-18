c706608ceb5084591c3ac38129c653f9cb9542c8
da593 <dylanau99@gmail.com>
2025-01-18 01:44:02 -0500

    Update resume link

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import data from '../components/projects/data.json';
 import ProjectCard from '../components/projects/ProjectCard';
 
-const resumeSrc = "https://drive.google.com/file/d/10uK3Y-9-k8HPNg8wROIHIPtSDiqj9SFw/view?usp=drive_link"
+const resumeSrc = "https://drive.google.com/file/d/1eF6VO09ld699O-4Gd-yot8tIyIioREtd/view?usp=drive_link"
 
 function Projects() {
     return (
